740e51f33c0592d1584e38bc28c90331e948479a
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-02 15:07:30 +0530

    menuOption = DetectMenuOption();

--- scalability-sample-2E/Program.cs
+++ scalability-sample-2E/Program.cs
@@ -10,11 +10,10 @@ namespace scalability_sample_2E
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
     }
 }
